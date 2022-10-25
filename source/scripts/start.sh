5b58c9c150012e95628beb4ce035186ae78caeaa
goflutterjava <goflutterjava@gmail.com>
2022-10-25 21:36:04 +0800

    fix es configfile

--- source/scripts/start.sh
+++ source/scripts/start.sh
@@ -3,8 +3,8 @@
 mkdir -p /opt/elasticsearch/logs
 echo '' > $ELASTICSEARCH_HOME/config/elasticsearch.yml
 echo -e 'xpack.security.enabled: false' > $ELASTICSEARCH_HOME/config/elasticsearch.yml
-echo -e 'xpack.security.transport.ssl.enabled: false' > $ELASTICSEARCH_HOME/config/elasticsearch.yml
-echo -e 'xpack.security.http.ssl.enabled: false' > $ELASTICSEARCH_HOME/config/elasticsearch.yml
+echo -e 'xpack.security.transport.ssl.enabled: false' >> $ELASTICSEARCH_HOME/config/elasticsearch.yml
+echo -e 'xpack.security.http.ssl.enabled: false' >> $ELASTICSEARCH_HOME/config/elasticsearch.yml
 nohup $ELASTICSEARCH_HOME/bin/elasticsearch -Ediscovery.type=single-node -Enetwork.host=0.0.0.0 >>/opt/elasticsearch/logs/elasticsearch.stdout.log 2>>/opt/elasticsearch/logs/elasticsearch.stderr.log &
 mkdir -p /opt/kibana/logs
 echo '' > $KIBANA_HOME/config/kibana.yml
