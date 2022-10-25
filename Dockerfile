f254f790828e19216aaa598f5ff155655948aa30
goflutterjava <goflutterjava@gmail.com>
2022-10-25 14:30:37 +0800

    try to reduce message size

--- Dockerfile
+++ Dockerfile
@@ -2,18 +2,18 @@ FROM ttbb/elasticsearch:nake AS elasticsearch
 
 FROM ttbb/kibana:nake
 
-COPY --chown=sh --from=elasticsearch /opt/sh/elasticsearch /opt/sh/elasticsearch
+COPY --chown=sh --from=elasticsearch /opt/elasticsearch /opt/elasticsearch
 
-COPY --chown=sh source /opt/sh
+COPY --chown=sh source /opt
 
-WORKDIR /opt/sh
+WORKDIR /opt
 
-ENV ELASTICSEARCH_HOME /opt/sh/elasticsearch
+ENV ELASTICSEARCH_HOME /opt/elasticsearch
 
-ENV KIBANA_HOME /opt/sh/kibana
+ENV KIBANA_HOME /opt/kibana
 
 USER sh
 
 EXPOSE 5601 9200 9300
 
-CMD ["/usr/bin/dumb-init", "bash", "-vx","/opt/sh/scripts/start.sh"]
+CMD ["/usr/bin/dumb-init", "bash", "-vx","/opt/scripts/start.sh"]
--- source/scripts/start.sh
+++ source/scripts/start.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-mkdir -p /opt/sh/logs
+mkdir -p /opt/elasticsearch/logs
 echo '' > $ELASTICSEARCH_HOME/config/elasticsearch.yml
 echo -e 'xpack.security.enabled: false' > $ELASTICSEARCH_HOME/config/elasticsearch.yml
 echo -e 'xpack.security.transport.ssl.enabled: false' > $ELASTICSEARCH_HOME/config/elasticsearch.yml
 echo -e 'xpack.security.http.ssl.enabled: false' > $ELASTICSEARCH_HOME/config/elasticsearch.yml
-nohup $ELASTICSEARCH_HOME/bin/elasticsearch -Ediscovery.type=single-node -Enetwork.host=0.0.0.0 >>/opt/sh/logs/elasticsearch.stdout.log 2>>/opt/sh/logs/elasticsearch.stderr.log &
+nohup $ELASTICSEARCH_HOME/bin/elasticsearch -Ediscovery.type=single-node -Enetwork.host=0.0.0.0 >>/opt/elasticsearch/logs/elasticsearch.stdout.log 2>>/opt/elasticsearch/logs/elasticsearch.stderr.log &
+mkdir -p /opt/kibana/logs
 echo '' > $KIBANA_HOME/config/kibana.yml
 echo -e 'server.host: "0.0.0.0"' > $KIBANA_HOME/config/kibana.yml
-nohup $KIBANA_HOME/bin/kibana >>/opt/sh/logs/kibana.stdout.log 2>>/opt/sh/logs/kibana.stderr.log &
+nohup $KIBANA_HOME/bin/kibana >>/opt/kibana/logs/kibana.stdout.log 2>>/opt/kibana/logs/kibana.stderr.log &
 tail -f /dev/null
