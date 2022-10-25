c04547b9eb3e3f9abb8a554b0fd0f067f419f9da
goflutterjava <goflutterjava@gmail.com>
2022-10-25 13:18:54 +0800

    disable es ssl by default

--- source/scripts/start.sh
+++ source/scripts/start.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 mkdir -p /opt/sh/logs
+echo '' > $ELASTICSEARCH_HOME/config/elasticsearch.yml
+echo -e 'xpack.security.enabled: false' > $ELASTICSEARCH_HOME/config/elasticsearch.yml
+echo -e 'xpack.security.transport.ssl.enabled: false' > $ELASTICSEARCH_HOME/config/elasticsearch.yml
+echo -e 'xpack.security.http.ssl.enabled: false' > $ELASTICSEARCH_HOME/config/elasticsearch.yml
 nohup $ELASTICSEARCH_HOME/bin/elasticsearch -Ediscovery.type=single-node -Enetwork.host=0.0.0.0 >>/opt/sh/logs/elasticsearch.stdout.log 2>>/opt/sh/logs/elasticsearch.stderr.log &
 echo '' > $KIBANA_HOME/config/kibana.yml
 echo -e 'server.host: "0.0.0.0"' > $KIBANA_HOME/config/kibana.yml
